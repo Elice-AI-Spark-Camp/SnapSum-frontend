f25554c030893954f6ae2f9c8ff2fae8a7e0ada8
songyeonji <dus3439@naver.com>
2025-02-13 21:23:50 +0900

    💄 design: 폰트설정 및 로고 svg 추가

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ['fitmon-bucket.s3.amazonaws.com'],
+  },
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+
+  compiler: {
+    // 배포 시 console.log 자동 제거
+    removeConsole:
+      process.env.NODE_ENV === 'production'
+        ? { exclude: ['error', 'warn'] }
+        : false,
+  },
 };
 
 export default nextConfig;
