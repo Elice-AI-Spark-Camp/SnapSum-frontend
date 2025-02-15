00e5e31d9defed6291392ccbeac49631c50a6f3f
songyeonji <dus3439@naver.com>
2025-02-16 00:37:44 +0900

    ✨ feat: 버튼 타입 추가

--- src/pages/text.tsx
+++ src/pages/text.tsx
@@ -17,16 +17,25 @@ export default function Text() {
       <div className="w-[600px] mx-auto flex flex-col items-center gap-8">
         {/* 프로그레스 바 섹션 */}
         <div className="w-[600px]  top-[96px] left-0 right-0 bg-white z-10">
-            <ProgressBar progress={67} variant="primary" />
-            <div className="h-6" /> {/* 간격 */}
-            <ProgressBar progress={67} variant="secondary" />
+          <ProgressBar progress={67} variant="primary" />
+          <div className="h-6" /> {/* 간격 */}
+          <ProgressBar progress={67} variant="secondary" />
         </div>
         <div className="flex gap-4">
           <VideoButton
-            onClick={() => console.log('영상 제작하기')}
+            variant="create"
+            onClick={() => console.log('create video')}
+          />
+          <VideoButton
+            variant="download"
+            onClick={() => console.log('download video')}
+          />
+          <VideoButton
+            variant="new"
+            onClick={() => console.log('new video')}
           />
         </div>
-        
+
         <div className="grid grid-cols-2 gap-4">
           <TTSButton
             onClick={() => setSelectedId('1')}
@@ -54,26 +63,26 @@ export default function Text() {
           />
         </div>
         <div>
-      {/* 단독 네비게이션 버튼 사용 */}
-      <div className="flex justify-between">
-        <NavigationButton 
-          direction="prev" 
-          onClick={() => console.log('prev')} 
-        />
-        <NavigationButton 
-          direction="next" 
-          onClick={() => console.log('next')} 
-        />
-      </div>
+          {/* 단독 네비게이션 버튼 사용 */}
+          <div className="flex justify-between">
+            <NavigationButton
+              direction="prev"
+              onClick={() => console.log('prev')}
+            />
+            <NavigationButton
+              direction="next"
+              onClick={() => console.log('next')}
+            />
+          </div>
 
-      {/* 채팅 메시지 사용 */}
-      <ChatMessage
-        message="안녕하세요, 저는 당신의 소중한 영상 제작을 AI로 도울 SNAPSUM 입니다. 블로그는 링크에 있는 글을 자동정리 부탁드려요."
-        showNavigationButtons
-        onPrevClick={() => console.log('prev')}
-        onNextClick={() => console.log('next')}
-      />
-    </div>
+          {/* 채팅 메시지 사용 */}
+          <ChatMessage
+            message="안녕하세요, 저는 당신의 소중한 영상 제작을 AI로 도울 SNAPSUM 입니다. 블로그는 링크에 있는 글을 자동정리 부탁드려요."
+            showNavigationButtons
+            onPrevClick={() => console.log('prev')}
+            onNextClick={() => console.log('next')}
+          />
+        </div>
       </div>
     </Layout>
   );
