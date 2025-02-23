4bb694d91eca5b91d6fe3347af7f9d35f84d21f4
songyeonji <dus3439@naver.com>
2025-02-23 17:22:44 +0900

    💬 createPR: api 연결 완료(요약, 문단 나누기 / tts, img api는 크로스 오류남)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
-import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
