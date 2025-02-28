7c36fae8da3998fa7eabb86f9028db57f956a045
songyeonji <dus3439@naver.com>
2025-03-01 06:00:08 +0900

    🐛 fix: 특정파일 빌드 조건 예외처리

--- src/pages/img.tsx
+++ src/pages/img.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // pages/img.tsx
 import { useEffect, useState, useRef } from 'react';
 import ImageGrid from '@/components/pages/img/ImageGrid';
